01d6454571660fa3a26b8911d8056dce3b860bad
Nikola <hana.dev22@gmail.com>
2024-01-11 13:57:11 -0500

    adding reorder pages

--- Backend/app.js
+++ Backend/app.js
@@ -45,6 +45,6 @@ app.post('/upload', upload.array("files"), (req, res) => {
 app.use('/pages', PagesRoutes);
 
 // Start the server
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+app.listen(8081, () => {
+  console.log('Server is running on port 8081');
 });
--- Backend/controllers/pages.controller.js
+++ Backend/controllers/pages.controller.js
@@ -77,7 +77,56 @@ const deletePages = async (req, res) => {
   }
 }
 const reorderPages = async (req, res) => {
+  try {
+    const uint8Array = fs.readFileSync(getCurrentFile());
+    console.log(uint8Array);
+    const pdfDoc = await PDFDocument.load(uint8Array);
 
+    const pageNumbers = req.body.newOrderPage;
+
+    for (let i = 0; i < pageNumbers.length; i++) {
+      pageNumbers[i] = parseInt(pageNumbers[i]) - 1;
+    }
+
+    console.log(typeof pageNumbers, ":", pageNumbers);
+
+    const reorderedPdfDoc = await PDFDocument.create();
+
+    // pageNumbers.forEach(async (pageNumber) => {
+    //   console.log(pageNumber);
+    //   // const page = pdfDoc.getPage(pageNumber - 1); // Page numbers are 0-indexed
+    //   const copiedPage = await reorderedPdfDoc.copyPages(pdfDoc, [pageNumber - 1])[0];
+    //   console.log(copiedPage);
+
+    //   reorderedPdfDoc.addPage(copiedPage);
+    // });
+
+    const copiedPages = await reorderedPdfDoc.copyPages(pdfDoc, pageNumbers)
+    for(let i = 0; i<pageNumbers.length; i++) {
+      reorderedPdfDoc.addPage(copiedPages[i]);
+    }
+
+    // Save the reordered PDF to a new file
+    const pdfBytes = await reorderedPdfDoc.save();
+
+    const filePath = 'save/save.pdf';
+
+    fs.writeFileSync(filePath, pdfBytes, (err) => {
+      if (err) {
+        console.error('Error while saving the file:', err);
+      } else {
+        console.log('The file has been saved successfully.');
+      }
+    })
+    res.send("Save");
+
+    // res.setHeader('Content- mbType', 'application/pdf');
+    // res.setHeader('Content-Disposition', 'attachment; filename=modified.pdf');
+    // res.send(Buffer.from(pdfBytes));
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Internal server error');
+  }
 }
 
 module.exports = {
